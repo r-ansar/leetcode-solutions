48177040e96a03e59a31d7fd2616ba46dff4f5c3
Raheem Ansar <raheemansar2006@gmail.com>
2026-04-08 04:07:40 +0500

    Added Reverse Linked List Solution

---
+++ 10_Linked_List/01_ReverseLinkedList.cpp
@@ -0,0 +1,30 @@
+// Statement
+
+// Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        while(curr!=NULL){
+            ListNode* temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+};
