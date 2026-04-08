1161dca40d46f0d383590390944b7d89a659b29a
Raheem Ansar <raheemansar2006@gmail.com>
2026-04-08 07:48:00 +0500

    Added Linked List Cycle Detection Solution

---
+++ 10_Linked_List/03_LinkedListCycleDetection.cpp
@@ -0,0 +1,24 @@
+// Statement
+
+// Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+// There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
+// Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+// Note: index is not given to you as a parameter.
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;  
+            }
+        }
+        return false;  
+    }
+};
